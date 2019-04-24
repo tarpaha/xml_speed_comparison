f5a70ba38b122837f4deb3db61334322681beef1
tarpaha <ppopchikovsky@gmail.com>
2019-04-25 00:35:19 +0300

    xml_rs_reader and simple profiling of two different xml readers

--- xml_reader/src/main.rs
+++ xml_reader/src/main.rs
@@ -2,17 +2,34 @@ use std::fs;
 use std::time::Instant;
 
 mod quick_xml_reader;
+mod xml_rs_reader;
 mod types;
 
 fn main() {
+    profile_quick_xml();
+    profile_xml_rs();
+ }
+
+fn profile_quick_xml() {
     let data = read_file_to_string("resource_map.xml");
 
     let now = Instant::now();
     let resource_map = quick_xml_reader::read(data.as_bytes());
     let elapsed = now.elapsed();
-    
+
     let sec = (elapsed.as_secs() as f64) + (elapsed.subsec_nanos() as f64 / 1000_000_000.0);
-    println!("Bundles count: {}, seconds: {}", resource_map.get_bundles_count(), sec);
+    println!("quick-xml => bundles count: {}, seconds: {}", resource_map.get_bundles_count(), sec);
+}
+
+fn profile_xml_rs() {
+    let data = read_file_to_string("resource_map.xml");
+
+    let now = Instant::now();
+    let resource_map = xml_rs_reader::read(data.as_bytes());
+    let elapsed = now.elapsed();
+
+    let sec = (elapsed.as_secs() as f64) + (elapsed.subsec_nanos() as f64 / 1000_000_000.0);
+    println!("xml-rs    => bundles count: {}, seconds: {}", resource_map.get_bundles_count(), sec);
 }
 
 fn read_file_to_string(filename: &str) -> String {
---
+++ xml_reader/src/xml_rs_reader.rs
@@ -0,0 +1,68 @@
+extern crate xml;
+
+use std::io::BufRead;
+use xml::reader::{EventReader, XmlEvent};
+
+use super::types;
+
+pub fn read<R: BufRead>(data: R) -> types::ResourceMap {
+    return read_resource_map(data).unwrap();
+}
+
+fn read_resource_map<R: BufRead>(data: R) -> Result<types::ResourceMap, String> {
+    let mut resource_map = types::ResourceMap::new();
+    let mut current_bundle: Option<types::Bundle> = None;
+    let parser = EventReader::new(data);
+    for event in parser {
+        match event {
+            Ok(XmlEvent::StartElement { name, attributes, .. }) => {
+                match name.local_name.as_str() {
+                    "Bundle" => {
+                        let name = attributes.iter().find(|x| x.name.local_name.as_str() == "Filename");
+                        if name == None {
+                            return Err("Cannot find attribute Filename in Bundle tag".to_string());
+                        }
+                        let size = attributes.iter().find(|x| x.name.local_name.as_str() == "DownloadSize");
+                        if size == None {
+                            return Err("Cannot find attribute DownloadSize in Bundle tag".to_string());
+                        }
+                        match size.unwrap().value.parse::<u32>() {
+                            Ok(size) => current_bundle = Some(types::Bundle::new(name.unwrap().value.to_string(), size)),
+                            Err(e) => return Err(e.to_string())
+                        }
+                    },
+                    "Asset" => {
+                        let path_attr = attributes.iter().find(|x| x.name.local_name.as_str() == "AssetPath");
+                        match path_attr {
+                            Some(path) => {
+                                match current_bundle {
+                                    Some(ref mut bundle) => bundle.add_asset(types::Asset::new(path.value.to_string())),
+                                    None => return Err("Found opening Asset tag out of Bundle scope".to_string())
+                                }
+                            }
+                            None => return Err("Cannot find attribute AssetPath in Asset tag".to_string())
+                        }
+                    },
+                    _ => {}
+                }
+            },
+            Ok(XmlEvent::EndElement { name }) => {
+                match name.local_name.as_str() {
+                    "Bundle" => {
+                        match current_bundle {
+                            Some(_) => {
+                                resource_map.add_bundle(current_bundle.unwrap());
+                                current_bundle = None;
+                            }
+                            None => return Err("Found closing Bundle tag without opening one".to_string())
+                        }
+                    },
+                    _ => {}
+                }
+            },
+            Err(e) => return Err(e.to_string()),
+            _ => {}
+        }
+    }
+    Ok(resource_map)
+}
